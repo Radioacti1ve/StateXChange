a6c2874a507c75141a3785be7e4664c26d818e9e
Maxim Kornev <radioactiive@yandex.ru>
2025-10-03 18:08:10 +0300

    исправляет тип

--- src/providers/ContextStoreProvider.tsx
+++ src/providers/ContextStoreProvider.tsx
@@ -2,22 +2,22 @@ import React, { useMemo, useReducer } from 'react';
 import { getCurrency } from '../api/getCurrency';
 import { currencyToItem } from '../utils';
 import type { SortState } from '../types';
-import { CurrencyContext, type IState } from '../stores/context/store';
+import { CurrencyContext, type IContextState } from '../stores/context/store';
 
 type Action =
   | { type: 'LOAD_START' }
-  | { type: 'LOAD_SUCCESS'; payload: IState['items'] }
+  | { type: 'LOAD_SUCCESS'; payload: IContextState['items'] }
   | { type: 'LOAD_ERROR'; payload: string }
   | { type: 'SET_SORT'; payload: SortState };
 
-const initialState: IState = {
+const initialState: IContextState = {
   items: [],
   isLoading: false,
   error: null,
   sort: { field: 'name', order: 'asc' },
 };
 
-function reducer(state: IState, action: Action): IState {
+function reducer(state: IContextState, action: Action): IContextState {
   switch (action.type) {
     case 'LOAD_START':
       return { ...state, isLoading: true, error: null };
